bac061eb8de8974b9477cf82b62a5bb87da1d633
JulioRFlores <77270469+JulioRFlores@users.noreply.github.com>
2022-02-05 19:50:24 -0800

    Add files via upload

--- lstore/page.py
+++ lstore/page.py
@@ -1,4 +1,4 @@
-
+MAX_ENTRIES = 512
 class Page:
 
     def __init__(self):
@@ -6,9 +6,26 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
+        return self.num_records <= MAX_ENTRIES
+
+    def read(self, location):
+        try:
+            if location < 512:
+                index  = location * 8
+                return int.from_bytes(self.data[index:index + 8], 'big')
+            return False 
+        except:
+            return False 
 
     def write(self, value):
-        self.num_records += 1
-        pass
+        
+        try:
+            if self.has_capacity():
+                index = self.num_records * 8
+                self.data[index:index + 8] = value.to_bytes( 8, 'big')
+                self.num_records += 1
+                return True    
+            return False 
+        except:
+            return False
 
